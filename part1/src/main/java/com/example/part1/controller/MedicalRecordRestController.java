176ae40b0f8d3b4cfc6ef78abe6326371f42fe43
AnuariteA <anuaritealberto@gmail.com>
2025-04-24 05:06:01 +0100

    Update MedicalRecordRestController.java

--- part1/src/main/java/com/example/part1/controller/MedicalRecordRestController.java
+++ part1/src/main/java/com/example/part1/controller/MedicalRecordRestController.java
@@ -1,13 +1,12 @@
 package com.example.part1.controller;
 
 import com.example.part1.domain.Appointments;
-import com.example.part1.domain.MedicalRecord;
+import com.example.part1.domain.Record;
 import com.example.part1.repo.AppointmentRepo;
 import com.example.part1.repo.MedicalRecordRepo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.sql.Timestamp;
 import java.util.Optional;
 import java.time.LocalDateTime;
@@ -26,7 +25,7 @@ public class MedicalRecordRestController {
 
     // === Endpoint #20: Create a medical record (POST /medical-records) ===
     @PostMapping
-    public ResponseEntity<Object> createMedicalRecord(@RequestBody MedicalRecord medicalRecord) {
+    public ResponseEntity<Object> createMedicalRecord(@RequestBody Record medicalRecord) {
         // Set creation timestamp
         medicalRecord.setRecordDate(Timestamp.valueOf(LocalDateTime.now()));
 
@@ -50,7 +49,7 @@ public class MedicalRecordRestController {
             return ResponseEntity.badRequest().body("Diagnosis is required."); // HTTP 400
         }
 
-        MedicalRecord saved = medicalRecordRepo.save(medicalRecord);
+        Record saved = medicalRecordRepo.save(medicalRecord);
         return ResponseEntity.status(HttpStatus.CREATED).body(saved); // HTTP 201
     }
 }
